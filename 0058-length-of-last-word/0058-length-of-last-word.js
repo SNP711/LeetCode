10c03b385045c308f1593fe9bedcf995e298805a
Sreeshanth Np <sreeshanthnp7@gmail.com>
2024-08-20 14:30:05 +0530

    Time: 43 ms (92.04%), Space: 48.9 MB (40.71%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    var arr = s.split(' ')
+    for(i=arr.length-1;i>=0;i--)
+    if(arr[i].length==0)
+    {
+        continue
+    }
+    else{
+        return arr[i].length
+    }
+
+};
